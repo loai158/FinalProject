abf4e2c445a667744e33cb1629c911eb91e15501
Mostafa Adry <mostafaelsheref256@gmail.com>
2025-06-03 17:52:56 +0200

    adding AllDoctor controll

--- FinalProject.App/Areas/Customer/Controllers/HomeController.cs
+++ FinalProject.App/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
+using Azure;
+using FinalProject.Core.Feature.Doctor.Query.Models;
 using FinalProject.Data.Models;
 using FinalProject.Services.Abstracts;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace FinalProject.App.Areas.Customer.Controllers
 {
@@ -12,18 +16,24 @@ namespace FinalProject.App.Areas.Customer.Controllers
         private readonly IDepartmentServices _departmentServices;
 
         private readonly ILogger<HomeController> _logger;
+        private readonly IMediator _mediator;
+        private readonly IDoctorServices _doctorServices;
 
-        public HomeController(ILogger<HomeController> logger, IDepartmentServices departmentServices)
+        public HomeController(ILogger<HomeController> logger, IMediator mediator, IDepartmentServices departmentServices, IDoctorServices doctorServices)
         {
+            _mediator = mediator;
             this._departmentServices = departmentServices;
+            this._doctorServices = doctorServices;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var Docs = await _mediator.Send(new GetAllDoctorsQuery { });
+            Docs = Docs.Skip((1 - 1) * 4).Take(4);
             var depts = _departmentServices.getAll().ToList();
             ViewData["Departments"] = depts;
-            return View();
+            return View(Docs);
         }
 
         public IActionResult Privacy()
